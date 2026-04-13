f7e02d5de5b5cde12b8693d5b7818e857a0a0ce0
LALITH KRISH <lalithkrish2006@gmail.com>
2026-04-13 10:29:34 +0530

    Time: 1021 ms (60.25%) | Memory: 21.8 MB (21.00%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,43 +1,31 @@
 class Solution:
     def sortArray(self, nums):
-        temp = [0] * len(nums)
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
 
-        def merge_sort(left, right):
-            if left >= right:
-                return
+            mid = len(arr) // 2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
 
-            mid = (left + right) // 2
-            merge_sort(left, mid)
-            merge_sort(mid + 1, right)
+            return merge(left, right)
 
-            merge(left, mid, right)
+        def merge(left, right):
+            i = j = 0
+            result = []
 
-        def merge(left, mid, right):
-            i = left
-            j = mid + 1
-            k = left
-
-            while i <= mid and j <= right:
-                if nums[i] <= nums[j]:
-                    temp[k] = nums[i]
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    result.append(left[i])
                     i += 1
                 else:
-                    temp[k] = nums[j]
+                    result.append(right[j])
                     j += 1
-                k += 1
 
-            while i <= mid:
-                temp[k] = nums[i]
-                i += 1
-                k += 1
+            # Add remaining elements
+            result.extend(left[i:])
+            result.extend(right[j:])
 
-            while j <= right:
-                temp[k] = nums[j]
-                j += 1
-                k += 1
+            return result
 
-            for i in range(left, right + 1):
-                nums[i] = temp[i]
-
-        merge_sort(0, len(nums) - 1)
-        return nums
+        return merge_sort(nums)
